bfbf2e83599e6e5fed32dd368438b6b350870a60
Rafael Carrasco <rscarrasco@proton.me>
2022-09-19 18:48:23 -0300

    Adds styling to the userpage

--- resources/views/user/userpage.blade.php
+++ resources/views/user/userpage.blade.php
@@ -1,25 +1,27 @@
 <x-layout>
   <h1>{{ $user['name'] }}'s faves</h1>
   @auth
-  <a href='/{{ $user['userhandle'] }}/faves/create'>New fave</a>
+  <a class='mb-3 btn btn-success' href='/{{ $user['userhandle'] }}/faves/create'>New fave</a>
   @endauth
   @if($faves)
     @foreach($faves as $fave)
-      <div>
-        <p>
-          <a target='_blank' href='{{ $fave->link }}'>{{ $fave->name }}</a>
-        </p>
-        <p>{{ $fave->tags }}</p>
-        <p>{{ $fave->description }}</p>
+      <div class='card mb-3'>
+        <div class='card-body'>
+          <h2 class='card-title'>
+            <a target='_blank' href='{{ $fave->link }}'>{{ $fave->name }}</a>
+          </h2>
+          <p>{{ $fave->tags }}</p>
+          <p>{{ $fave->description }}</p>
+        </div>
         @if($show_owner_tools)
-        <p>
-          <a href= '/{{ $user['userhandle'] }}/faves/{{ $fave->id }}/edit/'>Edit</a>
-          <form method='POST' action='/{{ $user['userhandle'] }}/faves/{{ $fave->id }}/remove'>
+        <div class='card-footer'>
+          <a class='btn btn-outline-primary me-3' href= '/{{ $user['userhandle'] }}/faves/{{ $fave->id }}/edit/'>Edit</a>
+          <form method='POST' action='/{{ $user['userhandle'] }}/faves/{{ $fave->id }}/remove' style='display:inline'>
             @csrf
             @method('DELETE')
-            <button type='submit'>Delete</button>
+            <button class='btn btn-outline-danger' type='submit'>Delete</button>
           </form>
-        </p>
+        </div>
         @endif
       </div>
     @endforeach
