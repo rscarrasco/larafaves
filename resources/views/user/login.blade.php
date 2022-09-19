ccd3af853aca1042ff9e18107c59c9979d56c81a
Rafael Carrasco <rscarrasco@proton.me>
2022-09-19 17:38:57 -0300

    ]Adds styling to login form

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -2,21 +2,23 @@
   <h1>Login</h1>
     <form method='POST' action='/authenticate'>
       @csrf
-      <div>
-        <label for='email'>Email</label>
-        <input type='text' name='email' value='{{ old('email') }}' />
+      <div class='mb-3'>
+        <label for='email' class='form-label'>Email</label>
+        <input type='text' name='email' value='{{ old('email') }}' class='form-control'/>
         @error('email')
-        <p>{{ $message }}</p>
+        <div >
+          <p class='form-text text-danger'>{{ $message }}</p>
+        </div>
         @enderror
       </div>
 
-      <div>
-        <label for='password'>Password</label>
-        <input type="password" name="password" />
+      <div class='mb-3'>
+        <label for='password' class='form-label'>Password</label>
+        <input type="password" name="password" class='form-control'/>
       </div>
 
-      <div>
-        <button type='submit'>Login</button>
+      <div class='mb-3'>
+        <button type='submit' class='btn btn-primary'>Login</button>
       </div>
     
     </form>
